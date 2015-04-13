9996172719b88970f569cf8c6bb33db01b49fe78
xiaopow <xiaoyang88@gmail.com>
2015-04-13 06:43:45 +0000

    fix conflict index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ var Hapi = require('hapi');
 var server = new Hapi.Server();
 
 server.connection({
-<<<<<<< HEAD
   host: '0.0.0.0',
   port: 8080,
   routes: {cors: true}
@@ -16,18 +15,7 @@ server.register(plugins, function (err) {
   server.start(function () {
     server.log('info', 'Server running at: ' + server.info.uri);
   });
-=======
-  host: 'localhost',
-  port: 8000
-});
 
-server.route({
-  method: 'GET',
-  path: '/hello',
-  handler: function (request,reply) {
-    reply('hello world');
-  }
->>>>>>> 95f40aa23a30fe0cd71ac7a06c9bd843a35333e5
 });
 
 server.start();
