e67a33640da8edb5691cbf3d0c33423ff5e63209
xiaopow <xiaoyang88@gmail.com>
2015-04-14 15:39:02 +0000

    change API to allow quieries to different collections

--- routes/quotes.js
+++ routes/quotes.js
@@ -13,10 +13,11 @@ exports.register = function(server, options, next) {
     // Get all the quotes
     {
       method: 'GET',
-      path: '/quotes',
+      path: '/{collect}',
       handler: function (request, reply) {
-        var db = request.server.plugins['hapi-mongodb'].db;
-        db.collection('quotes').find().toArray(function(err, result){
+        var collect = encodeURIComponent(request.params.collect);
+             var db = request.server.plugins['hapi-mongodb'].db;
+        db.collection(collect).find().toArray(function(err, result){
           if (err) throw err;
           reply(result);
         });
@@ -39,11 +40,12 @@ exports.register = function(server, options, next) {
     // POST a quote
     {
       method: 'POST',
-      path: '/quotes',
+      path: '/{collect}',
       handler: function (request, reply) {
+         var collect = encodeURIComponent(request.params.collect);
         var newQuote = request.payload.quote;
               var db = request.server.plugins['hapi-mongodb'].db;
-        db.collection('quotes').insert( newQuote, function(err, writeResult){
+        db.collection(collect).insert( newQuote, function(err, writeResult){
           if(err) {
             reply(Hapi.error.internal('Internal MongoDB Error', err));
           } else {
@@ -55,13 +57,14 @@ exports.register = function(server, options, next) {
     // PUT edit a quote
     {
       method: 'PUT',
-      path: '/quotes/{id}',
+      path: '/{collect}/{id}',
       handler: function (request, reply) {
+         var collect = encodeURIComponent(request.params.collect);
               var id = encodeURIComponent(request.params.id);
               var db = request.server.plugins['hapi-mongodb'].db;
         var ObjectID = request.server.plugins['hapi-mongodb'].ObjectID;
         var modQuote = request.payload.quote;
-        db.collection('quotes').update( {"_id" : ObjectID(id)}, { '$set': modQuote}, function(err, writeResult){
+        db.collection(collect).update( {"_id" : ObjectID(id)}, { '$set': modQuote}, function(err, writeResult){
           if(err) {
             reply(Hapi.error.internal('Internal MongoDB Error', err));
           } else {
@@ -73,12 +76,13 @@ exports.register = function(server, options, next) {
     // DELETE a quote
     {
       method: 'DELETE',
-      path: '/quotes/{id}',
+      path: '/{collect}/{id}',
       handler: function (request, reply) {
+         var collect = encodeURIComponent(request.params.collect);
               var id = encodeURIComponent(request.params.id);
               var db = request.server.plugins['hapi-mongodb'].db;
         var ObjectID = request.server.plugins['hapi-mongodb'].ObjectID;
-        db.collection('quotes').remove( {"_id" : ObjectID(id)}, function(err, writeResult){
+        db.collection(collect).remove( {"_id" : ObjectID(id)}, function(err, writeResult){
           if(err) {
             reply(Hapi.error.internal('Internal MongoDB Error', err));
           } else {
@@ -102,13 +106,14 @@ exports.register = function(server, options, next) {
         });
       }
     },
+    // search quotes with a keyword
     {
       method: 'GET',
       path: '/quotes/search/{keyword}',
       handler: function (request, reply) {
         var db = request.server.plugins['hapi-mongodb'].db; 
-        var keyword = encodeURIComponent(request.params.keyword);
-        db.collection('quotes').find({"quote": {$regex: keyword} }).toArray(function(err, result){
+        var keyword = { "$text": { "$search": request.params.keyword} };
+        db.collection('quotes').find(keyword).toArray(function(err, result){
           if (err) throw err;
           reply(result);
         });
